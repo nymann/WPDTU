c8f561b05b723154fa81f58170ca61fc4b0ec2e6
Jesper Mark <jespervmark@gmail.com>
2016-10-25 11:45:32 +0200

    Added data context in mainWindow. Added commands data bindings on two ribbon buttons. Added new commands for New and Open

--- UMLaut/App.xaml.cs
+++ UMLaut/App.xaml.cs
@@ -13,5 +13,7 @@ namespace UMLaut
     /// </summary>
     public partial class App : Application
     {
+
+
     }
 }
---
+++ UMLaut/ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace UMLaut.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+
+
+        
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+
+            this.LaunchNewInstance = new RelayCommand<object>(this.PerformLaunchNewInstance);
+            this.OpenFile = new RelayCommand<object>(this.PerformOpenFile);
+            this.TestDrawLine = new RelayCommand<object>(this.PerformTestDrawLine);
+
+
+            //if (IsInDesignMode)
+            //{
+            //    // Code runs in Blend --> create design time data.
+            //}
+            //else
+            //{
+            //    // Code runs "for real"
+            //}
+        }
+
+ 
+
+        public ICommand LaunchNewInstance { get; set; }
+        public ICommand OpenFile { get; set; }
+        public ICommand TestDrawLine { get; set; }
+
+
+        private void PerformLaunchNewInstance(object obj)
+        {
+            var app = new System.Diagnostics.ProcessStartInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            System.Diagnostics.Process.Start(app);
+        }
+
+        private void PerformOpenFile(object obj)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == true)
+            {
+
+            }
+        }
+
+        private void PerformTestDrawLine(object obj)
+        {
+            
+        }
+
+
+    }
+}
