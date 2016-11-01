9b83cdd9cddba51215cd768e6b628959c4cb32ec
jgilsaa <jacobgilsaa@hotmail.com>
2016-11-01 11:37:00 +0100

    added doc strings to the models

--- UMLaut/Model/Implementation/UMLLine.cs
+++ UMLaut/Model/Implementation/UMLLine.cs
@@ -10,17 +10,47 @@ namespace UMLaut.Model.Implementation
 {
     class UMLLine : ILine
     {
+        /// <summary>
+        /// Id of the starting shape
+        /// </summary>
         public Guid FromId { get; set; }
+        /// <summary>
+        /// Id of the end shape
+        /// </summary>
         public Guid ToId { get; set; }
+        /// <summary>
+        /// Label of the line
+        /// </summary>
         public string Label { get; set; }
+        /// <summary>
+        /// Type of the line
+        /// </summary>
         public ELine Type { get; }
 
 
+        /// <summary>
+        /// Default constructor for the line. 
+        /// </summary>
+        /// <param name="FromId"></param>
+        /// <param name="ToId"></param>
         public UMLLine(Guid FromId, Guid ToId)
         {
+            Type = ELine.Solid;
             this.FromId = FromId;
             this.ToId = ToId;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="FromId"></param>
+        /// <param name="ToId"></param>
+        /// <param name="Type"></param>
+        public UMLLine(Guid FromId, Guid ToId, ELine Type)
+        {
+            this.Type = Type;
+            this.FromId = FromId;
+            this.ToId = ToId;
+        }
     }
 }
