1aa2ea2f3b5599de3ec8a38692443d030b63e453
jgilsaa <jacobgilsaa@hotmail.com>
2016-11-15 09:47:35 +0100

    Issue 11: Added Shape & Line template selector

--- UMLaut/ViewModel/LineViewModel.cs
+++ UMLaut/ViewModel/LineViewModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UMLaut.Model.Enum;
+using UMLaut.Model.Implementation;
 
 namespace UMLaut.ViewModel
 {
     public class LineViewModel : BaseViewModel
     {
+
+        private UMLLine _line;
+
+        public LineViewModel(UMLLine line)
+        {
+            _line = line;
+        }
+
+        public ELine Type => _line.Type;
+
     }
 }
