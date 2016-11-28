647783f3e20b60d5ab47f1d3c5bcc17d2f9f085f
Nymann <s144833@student.dtu.dk>
2016-11-28 15:03:56 +0100

    AddLineCommand.cs added (preparing for lines to be implemented.)

---
+++ UMLaut/UndoRedo/AddLineCommand.cs
@@ -0,0 +1,28 @@
+﻿using UMLaut.ViewModel;
+
+namespace UMLaut.UndoRedo
+{
+    public class AddLineCommand : IUndoRedoCommand
+    {
+        private readonly LineViewModel _selectedElement;
+        private readonly MainViewModel _mainViewModel;
+
+        public AddLineCommand(LineViewModel selectedElement, MainViewModel mainViewModel)
+        {
+            _selectedElement = selectedElement;
+            _mainViewModel = mainViewModel;
+        }
+
+        public void Execute()
+        {
+            // Undo
+            _mainViewModel.Lines.Remove(_selectedElement);
+        }
+
+        public void UnExecute()
+        {
+            // Redo
+            _mainViewModel.Lines.Add(_selectedElement);
+        }
+    }
+}
