2af1adf090ca3fe6b7db512b89d4e7f4d4bd8ef5
Vorenius <jonatan.mohr@gmail.com>
2016-12-07 20:30:34 +0100

    Setfocus on Adorners and temporarily use MergeAdorner for all shapes

--- UMLaut/Services/Adorners/MergeAdorner.cs
+++ UMLaut/Services/Adorners/MergeAdorner.cs
@@ -40,6 +40,7 @@ namespace UMLaut.Services.Adorners
             ShapeViewModel shape = ele.DataContext as ShapeViewModel;
             shape.OffsetX = 0;
             shape.OffsetY = -shape.Height/2;
+            SetFocus(ref Top);
 
         }
         void HandleBottom(object sender, MouseButtonEventArgs args)
@@ -48,7 +49,8 @@ namespace UMLaut.Services.Adorners
             ShapeViewModel shape = ele.DataContext as ShapeViewModel;
             shape.OffsetX = 0;
             shape.OffsetY = shape.Height/2;
-
+            SetFocus(ref Bottom);
+            
         }
         void HandleLeft(object sender, MouseButtonEventArgs args)
         {
@@ -56,7 +58,8 @@ namespace UMLaut.Services.Adorners
             ShapeViewModel shape = ele.DataContext as ShapeViewModel;
             shape.OffsetX = -shape.Width / 2;
             shape.OffsetY = 0;
-
+            SetFocus(ref Left);
+            
         }
         void HandleRight(object sender, MouseButtonEventArgs args)
         {
@@ -64,17 +67,27 @@ namespace UMLaut.Services.Adorners
             ShapeViewModel shape = ele.DataContext as ShapeViewModel;
             shape.OffsetX = shape.Width / 2;
             shape.OffsetY = 0;
-
-        }
+            SetFocus(ref Right);
+                    }
         protected override Size ArrangeOverride(Size finalSize)
         {
             Top.Arrange(new Rect(ActualWidth / 2 - Constants.CornerBoxSize / 2, -Constants.CornerBoxSize, Constants.CornerBoxSize, Constants.CornerBoxSize));
             Bottom.Arrange(new Rect(ActualWidth / 2 - Constants.CornerBoxSize / 2, ActualHeight, Constants.CornerBoxSize, Constants.CornerBoxSize));
             Left.Arrange(new Rect(- Constants.CornerBoxSize, ActualHeight/2 - Constants.CornerBoxSize / 2, Constants.CornerBoxSize, Constants.CornerBoxSize));
             Right.Arrange(new Rect(ActualWidth, ActualHeight / 2 - Constants.CornerBoxSize / 2, Constants.CornerBoxSize, Constants.CornerBoxSize));
-
+            
             // Return the final size.
             return finalSize;
+        }
+
+        void SetFocus(ref Thumb focus)
+        {
+            Right.Background = Constants.CornerBoxColor;
+            Left.Background = Constants.CornerBoxColor;
+            Top.Background = Constants.CornerBoxColor;
+            Bottom.Background = Constants.CornerBoxColor;
+
+            focus.Background = Constants.CornerBoxColorFocus;
         }
         void BuildConnectionPoint(ref Thumb point)
         {
